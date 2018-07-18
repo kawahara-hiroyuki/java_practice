0af8b33ef9f687098c494f390a0187de462f8e78
川原拓之 <kawahara_hiroyuki@kawaharatakunonoMacBook-Air.local>
2018-07-19 01:07:57 +0900

    fix: lifegameの完成

--- lifegame.java
+++ lifegame.java
@@ -6,9 +6,10 @@ public class lifegame {
 
 	public static void main(String args[]) {
 		int rows = 16, columns = 16;
+		double spawnRate = 0.3;
 		
 		// first state
-		int[][] firstStateArr = makeFirstStateArr(rows, columns);
+		int[][] firstStateArr = makeFirstStateArr(rows, columns, spawnRate);
 		showStateArr(firstStateArr, rows, columns);
 
 		// next state
@@ -21,6 +22,7 @@ public class lifegame {
 
 		int[][] stateArr = firstStateArr;
 		int[][] nextStateArr = new int[rows][columns]; 
+		int[][] result = new int[rows][columns];
 
 		int i = 0;
 		for (i = 0; i < gameTimes; i++) {
@@ -32,7 +34,6 @@ public class lifegame {
 			showStateArr(nextStateArr, rows, columns);
 
 			stateArr = nextStateArr;
-
 		}
 	}
 
@@ -42,7 +43,7 @@ public class lifegame {
 		System.out.println();
 
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(200);
 		}catch(InterruptedException e){}
 	}
 
@@ -50,71 +51,86 @@ public class lifegame {
 	public static int[][] makeNextStateArr(int[][] stateArr, int rows, int columns) {
 		
 		int[][] nextStateArr = new int[rows][columns];
+		int[][] result = new int[rows][columns];
 
 		int i = 0, j = 0;
 		int temp = 0;
 
+
+		int column_minous_1 = columns - 1;
+
 		for (i = 0; i < columns; i++) {
-			switch (i):{
-				case 0:
-					nextStateArr[0][i] = stateArr[0][1] + stateArr[1][0];
-					break;
-				case columns - 1:
-					nextStateArr[0][i] = stateArr[0][i - 1] + stateArr[1][i];
-					break;
-				default:
-					nextStateArr[0][i] = stateArr[0][i - 1] + stateArr[1][i] + stateArr[0][i + 1];
+			if (i == 0) {
+				nextStateArr[0][i] = stateArr[0][1] + stateArr[1][0] + stateArr[1][1];
+			} else if (i == (columns - 1)) {
+				nextStateArr[0][i] = stateArr[0][i - 1] + stateArr[1][i] + stateArr[1][i - 1];
+			} else {
+				nextStateArr[0][i] = stateArr[0][i - 1] + stateArr[1][i - 1] + stateArr[1][i] + stateArr[1][i + 1] + stateArr[0][i + 1];
 			}
 		}
+
+		for (i = 1; i < rows - 1; i++) {
+			for (j = 0; j < columns; j++) {
+				if (j == 0) {
+					nextStateArr[i][j] = stateArr[i - 1][j] + stateArr[i - 1][j + 1] + stateArr[i][j + 1] + stateArr[i + 1][j + 1] + stateArr[i + 1][j];
+				} else if (j == (columns - 1)) {
+					nextStateArr[i][j] = stateArr[i - 1][j] + stateArr[i - 1][j - 1] +  stateArr[i][j - 1] + stateArr[i + 1][j - 1] + stateArr[i + 1][j];
+				} else {
+					nextStateArr[i][j] = stateArr[i - 1][j - 1] + stateArr[i][j - 1] + stateArr[i + 1][j - 1] + stateArr[i - 1][j] + stateArr[i + 1][j] + stateArr[i - 1][j + 1] + stateArr[i][j + 1] + stateArr[i + 1][j + 1]; 
+				}
+			}
+		}
+
+		for (i = 0; i < columns; i++) {
+			if (i == 0) {
+				nextStateArr[rows - 1][i] = stateArr[rows - 2][0] + stateArr[rows - 2][1] + stateArr[rows - 1][1];
+			} else if (i == (columns - 1)) {
+				nextStateArr[rows - 1][i] = stateArr[rows - 1][i - 1] + stateArr[rows - 2][i - 1] + stateArr[rows - 2][i];
+			} else {
+				nextStateArr[rows - 1][i] = stateArr[rows - 1][i - 1] + stateArr[rows - 2][i - 1] + stateArr[rows - 2][i] + stateArr[rows - 2][i + 1] + stateArr[rows - 1][i + 1];
+			}
+		}
+
+		result = convertArrToBoolean(stateArr, nextStateArr, rows, columns);	
+
+		return result;
+	}
+
+	// nextstatearrには周囲の黒ますの数が入っているので、その値を次世代の値に変化させる
+	public static int[][] convertArrToBoolean(int[][] stateArr, int[][] nextStateArr, int rows, int columns) {
+		int i = 0, j = 0;
+		int spawnNum_2 = 2, spawnNum_3 = 3;
+		int[][] result = new int[rows][columns];
 
 		for (i = 0; i < rows; i++) {
 			for (j = 0; j < columns; j++) {
-				switch (j):{
+				switch (stateArr[i][j]) {
+
 					case 0:
-						nextStateArr[i][j] = stateArr[i - 1][j] + stateArr[i][j + 1] + stateArr[i + 1][j];
+					// 誕生（＝白→黒になる）条件：白いマスの周囲に3つの黒いマスがあれば次の世代では黒いマスになる。
+						if(nextStateArr[i][j] == spawnNum_3) {
+							result[i][j] = 1;
+						} else {
+							result[i][j] = 0;
+						}
 						break;
-					case columns - 1:
-						nextStateArr[i][j] = stateArr[i - 1][j] + stateArr[i][j - 1] +  stateArr[i + 1][j];
+					case 1:
+						// 維持（＝黒→黒のまま）条件：黒いマスの周囲に2つか3つの黒いマスがあれば次の世代でも黒いマスになる。
+						if(nextStateArr[i][j] == spawnNum_2 || nextStateArr[i][j] == spawnNum_3) {
+							result[i][j] = 1;
+						} else {
+							result[i][j] = 0;
+						}
 						break;
-					default:
-						nextStateArr[i][j] = stateArr[i - 1][j] + stateArr[i + 1][j] + stateArr[i][j - 1] + stateArr[i][j + 1];
 				}
-
-			
 			}
 		}
-		
-
-		for (i = 0; i < columns; i++) {
-			switch (i):{
-				case 0:
-					nextStateArr[rows - 1][i] = stateArr[rows - 1][1] + stateArr[rows - 2][0];
-					break;
-				case columns - 1:
-					nextStateArr[rows - 1][i] = stateArr[rows - 1][i - 1] + stateArr[rows - 2][i];
-					break;
-				default:
-					nextStateArr[rows - 1][i] = stateArr[rows - 1][i - 1] + stateArr[rows - 1][i] + stateArr[rows - 1][i + 1];
-			}
-		}
-
-
-
-
-		
-		
-		return stateArr;
+	
+		return result;
 	}
 
-
-
-
-
-
 	// 初期状態の配列の作成
-	public static int[][] makeFirstStateArr(int rows, int columns) {
-		double spawnRate = 0.2;
-
+	public static int[][] makeFirstStateArr(int rows, int columns, double spawnRate) {
 		int[][] result = new int[rows][columns];
 		
 		Random rand = new Random();
@@ -137,9 +153,10 @@ public class lifegame {
 	// データの表示 
 	public static void showStateArr(int stateArr[][], int rows, int columns) {
 		int i = 0, j = 0;
+
 		for (i = 0; i < rows; i++) {
 			for ( j = 0; j < columns; j++) {
-				if (stateArr[i][j] == 1 ) {
+					if (stateArr[i][j] == 1 ) {
 					System.out.print("￭");
 				} else {
 					System.out.print(" ");
@@ -148,8 +165,6 @@ public class lifegame {
 			System.out.println();
 		}
 	}
-
-
 }
 
 
